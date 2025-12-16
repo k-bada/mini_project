145f69185071d0471c896a87b2b8cc1f65f42948
k-bada <rlaqkek319@gmail.com>
2025-12-17 01:14:34 +0900

    design roomPopup

--- src/main/java/domain/Room.java
+++ src/main/java/domain/Room.java
@@ -5,7 +5,7 @@ public class Room {
     private String title;
     private Player blackPlayer;
     private Player whitePlayer;
-
+    private String mode;        //30s, 60s 추가
     private String[][] board; // 15x15
     private String currentTurn;
     private String gameStatus;
